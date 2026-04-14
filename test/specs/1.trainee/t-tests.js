24ac06745234cdaebc2bfb8c5aeb5330e7dc229f
unknown <chvyaka.kk@gmail.com>
2026-04-14 15:30:10 +0300

    trainee tasks are done

--- test/specs/1.trainee/t-tests.js
+++ test/specs/1.trainee/t-tests.js
@@ -1,15 +1,88 @@
-import { expect } from '@wdio/globals'
+import { browser, expect } from '@wdio/globals'
 
 describe('ChatGPT generated trainee tests for knowledge consolidation', () => {
     it('Should log in correctly (example 1)', async () => {
-        
+        await browser.url('https://www.saucedemo.com/')
+
+        let usernameInput = await $('#user-name')
+        let passwordInput = await $('#password')
+        const loginButton = await $('#login-button')
+
+        await usernameInput.setValue('standard_user')
+        await passwordInput.setValue('secret_sauce')
+
+        await loginButton.click()
+
+        const currentURL = await browser.getUrl()
+        expect(currentURL).toBe('https://www.saucedemo.com/inventory.html')
+    })
+
+    it('Should show error when credentials are incorrect (example 2)', async () => {
+        await browser.url('https://www.saucedemo.com/')
+
+        let usernameInput = await $('#user-name')
+        let passwordInput = await $('#password')
+        const loginButton = await $('#login-button')
+
+        await usernameInput.setValue('standard_user')
+        await passwordInput.setValue('secret_sauSe')
+
+        await loginButton.click()
+
+        const errorMessage = await $('[class="error-message-container error"]')
+        expect (await errorMessage.isDisplayed()).toBe(true)
+    })
+
+    it('Should show page title when logged in (example 3)', async () => {
+        await browser.url('https://www.saucedemo.com/')
+
+        let usernameInput = await $('#user-name')
+        let passwordInput = await $('#password')
+        const loginButton = await $('#login-button')
+        await usernameInput.setValue('standard_user')
+        await passwordInput.setValue('secret_sauce')
+        await loginButton.click()
+
+        const welcomeCaption = await $('.app_logo')
+        expect(await welcomeCaption.getText()).toBe('Swag Labs')
+    })
+
+    it('Should count shown products on page (example 4)', async () => {
+        await browser.url('https://www.saucedemo.com/')
+
+        let usernameInput = await $('#user-name')
+        let passwordInput = await $('#password')
+        const loginButton = await $('#login-button')
+        await usernameInput.setValue('standard_user')
+        await passwordInput.setValue('secret_sauce')
+        await loginButton.click()
+
+        let productShown = await $$('.inventory_item')
+        expect(await productShown.length).toBe(6)
+    })
+
+    it('Should show product page after click (example 5)', async () => {
+        await browser.url('https://www.saucedemo.com/')
+
+        let usernameInput = await $('#user-name')
+        let passwordInput = await $('#password')
+        const loginButton = await $('#login-button')
+        await usernameInput.setValue('standard_user')
+        await passwordInput.setValue('secret_sauce')
+        await loginButton.click()
+
+        let productShown = await $$('.inventory_item_name')
+        await productShown[0].click()
+
+        const currentURL = await browser.getUrl()
+        expect(currentURL).toHaveText('/inventory-item.html?id=')
     })
 })
 
 /* Сгенерировалл задание в ChatGPT для консолидации знаний и использовании в резюме
 TRAINEE (база, освоение WebdriverIO)
 👉 цель: научиться локаторам, кликам, expect
-
+.inventory_item
 1️⃣ Логин (успешный)
 открыть сайт
 ввести:
